46c619bccbea4707fea3a285fef04fed0b993520
anchorchau <ashchau94.it@gmail.com>
2022-12-14 18:36:49 +0800

    .

--- read_yml_file/main.py
+++ read_yml_file/main.py
@@ -1,4 +1,3 @@
-import os
 import yaml
 
 with open('example.yml', 'r') as yml:
@@ -9,7 +8,7 @@ print('YAML parameters will be loaded as dictionary as below:')
 print(params)
 
 print('We can also output the values whether they are nested parameters.')
-print(f"{params['person']['name']} is a {params['person']['age']} {params['person']['gender']}.")
+print(f"{params['person']['name']} is a {params['person']['age']} years old {params['person']['gender']}.")
 
 if params['person']['gender'] == 'male':
     subject = 'his'
